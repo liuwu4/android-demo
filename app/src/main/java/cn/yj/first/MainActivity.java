3d234b0f310a82bc72a544fdc3486481022a5cad
yj <liuwu4@yeah.net>
2020-05-27 15:37:05 +0800

    refactor: 更换logo,app名称

--- app/src/main/java/cn/yj/first/MainActivity.java
+++ app/src/main/java/cn/yj/first/MainActivity.java
@@ -20,40 +20,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.intent);
-        click(button);
-    }
-
-    public void click(Button btn) {
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent("cn.yj.store.ACTION_START");
-                intent.addCategory("cn.yj.store.DEFAULT");
-                intent.putExtra("title", "Hello World");
-                startActivityForResult(intent, 1);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case 1:
-                setButton(requestCode, data);
-                break;
-            default:
-                Toast.makeText(this, "没有符合要求的选项!", Toast.LENGTH_LONG).show();
-                break;
-        }
-    }
-
-    public void setButton(int code, Intent data) {
-        if (code == 1) {
-            String info = data.getStringExtra("back");
-            button.setText(info);
-            return;
-        }
-        Toast.makeText(this, "不符合要求", Toast.LENGTH_LONG).show();
     }
 }
