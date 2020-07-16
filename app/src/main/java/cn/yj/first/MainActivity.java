fd079d55bceb877287c730868deebf8921d35d18
yj <liuwu4@yeah.net>
2020-07-16 11:35:09 +0800

    feat: 测试JetPack

--- app/src/main/java/cn/yj/first/MainActivity.java
+++ app/src/main/java/cn/yj/first/MainActivity.java
@@ -2,7 +2,13 @@ package cn.yj.first;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+
+import androidx.databinding.DataBindingUtil;
+
+import cn.yj.dao.Person;
+import cn.yj.first.databinding.DataBindTestBinding;
+import cn.yj.first.databinding.ViewBindTestBinding;
 
 /**
  * 主页
@@ -10,12 +16,26 @@ import android.widget.Button;
  * @author yujie
  */
 public class MainActivity extends Activity {
-Button textView;
+    private final static String TAG = MainActivity.class.getName();
+    private DataBindTestBinding dataBindBinding;
+    private ViewBindTestBinding viewBindTestBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_main);
-        textView = findViewById(R.id.main);
+//        addViewBinding();
+        addDataBinding();
+    }
 
+    private void addDataBinding() {
+        dataBindBinding = DataBindingUtil.setContentView(this, R.layout.data_bind_test);
+        Person person = new Person("李", "四", 20);
+        dataBindBinding.setPerson(person);
+    }
+
+    private void addViewBinding() {
+        viewBindTestBinding = ViewBindTestBinding.inflate(getLayoutInflater());
+        View view = viewBindTestBinding.getRoot();
+        setContentView(view);
     }
 }
---
+++ app/src/main/java/cn/yj/first/TextListView.java
@@ -0,0 +1,13 @@
+package cn.yj.first;
+
+public class TextListView {
+    String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
