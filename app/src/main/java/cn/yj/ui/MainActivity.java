625c94be6b18d26b01e734a94e464caaef0df357
yj <liuwu4@yeah.net>
2020-11-18 18:12:48 +0800

    feat: 添加微信sdk

--- app/src/main/java/cn/yj/ui/MainActivity.java
+++ app/src/main/java/cn/yj/ui/MainActivity.java
@@ -3,6 +3,8 @@ package cn.yj.ui;
 import android.app.Activity;
 import android.os.Bundle;
 
+import cn.yj.R;
+
 /**
  * 主页
  *
@@ -14,6 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_bind_test);
     }
 
 }
